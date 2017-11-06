05c17284c6ab98d0cb1b7c7500ebaedcb6d1864d
Jose Leon <leonj1@gmail.com>
2017-11-05 22:09:06 -0500

    working sample

--- example1/main.go
+++ example1/main.go
@@ -13,31 +13,41 @@ func callback(id string) {
 }
 
 func main() {
-	menu := climenu.NewButtonMenu("Welcome", "Where do you want to go today?")
-	menu.AddMenuItem("SparkJob", "sparkjob")
-	menu.AddMenuItem("PythonJob", "pythonjob")
+	step1 := climenu.NewButtonMenu("Welcome", "Where do you want to go today?")
+	step1.AddMenuItem("SparkJob", "sparkjob")
+	step1.AddMenuItem("PythonJob", "pythonjob")
 
-	action, escaped := menu.Run()
+	crd, escaped := step1.Run()
 	if escaped {
 		os.Exit(0)
 	}
+	fmt.Println("Selected Resource >", crd)
 
+	step2 := climenu.NewButtonMenu("Welcome", "Action to be performed?")
+	step2.AddMenuItem("Create", "create")
+	step2.AddMenuItem("Delete", "delete")
+	step2.AddMenuItem("List All", "list")
+	step2.AddMenuItem("Inspect", "inspect")
+
+	action, escaped := step2.Run()
+	if escaped {
+		os.Exit(0)
+	}
 	fmt.Println("action >", action)
 
-	checkbox := climenu.NewCheckboxMenu("Let's try some checkboxes",
+	checkbox := climenu.NewCheckboxMenu("Which job would you like to inspect?",
 		"Select options", "OK", "Cancel")
-	checkbox.AddMenuItem("Apples", "apples")
-	checkbox.AddMenuItem("Oranges", "oranges")
-	checkbox.AddMenuItem("Bananas", "bananas")
+	checkbox.AddMenuItem("py-abcd", "py-abcd")
+	checkbox.AddMenuItem("py-cdef", "py-cdef")
+	checkbox.AddMenuItem("py-ghij", "py-ghij")
 
 	selection, escaped := checkbox.Run()
 	if escaped {
 		os.Exit(0)
 	}
-
 	fmt.Println("selected >", selection)
 
-	response := climenu.GetText("Say something interesting", "hi")
+	response := climenu.GetText("Provide a jar uri", "")
 	if escaped {
 		os.Exit(0)
 	}
